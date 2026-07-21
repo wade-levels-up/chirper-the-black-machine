fd46b65a7cc6a09393d32123692e0cd972aa9be8
Wade Foster The Black Machine <172018441+wade-levels-up@users.noreply.github.com>
2026-07-21 20:34:58 +1000

    My second chirper app

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-slot:title>
+        About
+    </x-slot:title>
+
+    <div>
+        <a href="/">Home</a>
+        <h1>Welcome to the About Page</h1>
+        <p>This is my first Laravel application!</p>
+    </div>
+</x-layout>
