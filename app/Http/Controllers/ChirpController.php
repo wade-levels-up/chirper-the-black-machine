2e920f2890f1efd80756dd2d13f867a9e556dfb8
Wade Foster The Black Machine <172018441+wade-levels-up@users.noreply.github.com>
2026-07-27 21:02:46 +1000

    feat: Add edit and delete functionality

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $chirps = Chirp::with('user') // eager loading user with the chirp
+            ->latest()
+            ->take(50)
+            ->get();
+
+        $totalChirps = Chirp::count();
+
+        return view('home', [
+            'chirps' => $chirps,
+            'totalChirps' => $totalChirps
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ], [
+            'message.required' => 'Please write something to chirp!',
+            'message.max' => 'Chirps must be 255 characters or less.',
+        ]);
+
+        Chirp::create([
+            'message' => $validated['message'],
+            'user_id' => null,
+        ]);
+
+        return redirect('/')->with('success', 'Your chirp has been posted!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        // return view('edit', [
+        //     'chirp' => $chirp
+        // ]);
+        return view('edit', compact('chirp'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+        // if ($request->user()->cannot('update', $chirp)) {
+        //     abort(403);
+        // }
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ], [
+            'message.required' => 'Please write something to chirp!',
+            'message.max' => 'Chirps must be 255 characters or less.',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect('/')->with('success', 'Chirp updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        $chirp->delete();
+
+        return redirect('/')->with('success', 'Chirp deleted!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ChirpController::class, 'index']);
+Route::post('/chirps', [ChirpController::class, 'store']);
+Route::get('/about', [ChirpController::class, 'about']);
+Route::get('chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+Route::patch('/chirps/{chirp}', [ChirpController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 
-Route::get('/about', function () {
-    return view('about');
-});
+// Laravel Shorthand for the same as above:
+// Route::resource('chirps', ChirpController::class)
+// ->only(['store', 'edit', 'update', 'destroy']);
